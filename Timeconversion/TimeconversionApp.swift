a2e656c061367ab8a1d6eb8916c49e58e88772c7
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-20 18:34:15 +0100

    Initial Commit

---
+++ Timeconversion/TimeconversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeconversionApp.swift
+//  Timeconversion
+//
+//  Created by Aaron Thompson on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeconversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
