41a40ce9127398453afeadddaaffe780f72d059a
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-20 18:41:11 +0100

    Init commit. I chose the time conversion challenge which converts days, hours and minutes into seconds

--- Timeconversion/ContentView.swift
+++ Timeconversion/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var days = 0
+    @State private var hours = 0
+    @State private var minutes = 0
+    
+    var inSeconds: Int {
+        
+        let daystoSeconds = days * 24 * 60 * 60
+        let hoursToSeconds = hours * 60 * 60
+        let minutesToSeconds = minutes * 60
+        
+        return minutesToSeconds + hoursToSeconds + daystoSeconds
+    }
+    
+    func clearTime(){
+        days = 0
+        hours = 0
+        minutes = 0
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            Form{
+                Section{
+                    
+                    Picker("No. of days", selection: $days){
+                        ForEach(0...365, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    
+                    Picker("No. of hours", selection: $hours){
+                        ForEach(0...24, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    
+                    Picker("No. of minutes", selection: $minutes){
+                        ForEach(0...60, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    
+                }
+                
+                Section( header: Text("Converted to seconds") ){
+                    Text("\(inSeconds) seconds")
+                }
+                
+                
+            }.navigationTitle("Time Convertor").toolbar{
+                Button("Clear"){
+                    clearTime()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
